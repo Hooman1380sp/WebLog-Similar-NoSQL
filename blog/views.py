166d0c2184fddddd6b6daab64f4f4ce14c91f116
Hooman.sp <1380hz1380hz@gmail.com>
2023-06-07 16:04:23 +0330

    add all project in repository

---
+++ blog/views.py
@@ -0,0 +1,144 @@
+from django.shortcuts import render
+from datetime import date
+
+# Create your views here.
+
+all_post =[
+    {
+        'slug': 'German-Esat-UE',
+        'title': 'German.Esat UE',
+        'author': 'Hooman Sepehrirad',
+        'image': 'IMG_20180619_190926.jpg',
+        'data': date(2022,4,23),
+        'short_description': 'this is after Scene Battle 1941',
+        'content': '''n Vienna, where he had been living in relative poverty since 1907, Hitler received the final part of his father's estate in May 1913 and moved to Munich, 
+        
+where he earned money painting architectural scenes. He may have left Vienna to evade conscription into the Austrian Army.[3] Hitler later claimed that he did not wish to serve the Habsburg Empire because of the mixture of "races" in its army. The Bavarian police sent him back to Salzburg for induction into the Austrian Army, but he failed his physical exam on 5 February 1914 and returned to Munich.[3]
+
+He was 25 years old in August 1914, when Austria-Hungary and the German Empire entered the First World War. Because of his Austrian citizenship, he had to request permission to serve in the Bavarian Army. Permission was granted.[4] On the evidence of a report by the Bavarian authorities in 1924, which questioned how Hitler was allowed to serve in the Bavarian Army, 
+
+Hitler almost certainly was enlisted through an error on the part of the government. The authorities could not explain why he was not deported back to Austria in 1914 after he failed his physical exam for the Austrian Army. They concluded that the matter of Hitler's citizenship was simply not raised; thus he was allowed to enter the Bavarian Army.[5] In the army, Hitler continued to put forth his German nationalist ideas which he developed from a young age.[6]
+
+During the war, Hitler served in France and Belgium in the Bavarian Reserve Infantry Regiment 16 (1st Company of the List Regiment).[7][8] He was an infantryman in the 1st Company during the First Battle of Ypres (October 1914), which Germans remember as the Kindermord bei Ypern (Ypres Massacre of the Innocents) because approximately 40,000 men (between a third and a half, many of them university students) of nine newly-enlisted infantry divisions became casualties in the first twenty days. 
+
+Hitler's regiment entered the battle with 3,600 men but at its end mustered only 611 men.[9] By December, Hitler's own company of 250 was reduced to 42. Biographer John Keegan claims that this experience drove Hitler to become aloof and withdrawn for the remaining years of war.[10] After the battle, Hitler was promoted from Schütze (private) to Gefreiter (lance corporal). He was assigned to be a regimental message-runner.[11][12]
+
+Some have regarded this assignment as "a relatively safe job", because regimental headquarters was often several miles behind the front.[13] According to Thomas Weber, earlier historians of the period had not distinguished between regimental runners, who were based away from the front "in relative comfort", and company, or battalion runners, who moved among the trenches and were more often under fire.[13]
+
+Messengers' duties changed as the German Army on the Western Front settled into their defensive positions as a result of the ongoing stalemate. Fewer messages went by foot or bicycle and more by telephone. Hitler's circle of comrades also served at headquarters. They laughed at "Adi" for his aversion to smutty stories, and traded their jam rations for his tobacco.[A 2]
+
+In early 1915, Lance Corporal Hitler adopted a stray dog he named Fuchsl (Little Fox), who was taught many tricks and became his companion. Hitler described him as a "proper circus dog". In August 1917 the List Regiment transferred to a quiet sector of the front in Alsace. During the journey, both Fuchsl and Hitler's portfolio of sketches and paintings were stolen.[15] Hitler, though heartbroken by his loss, did take his first leave, which consisted of an 18-day visit to Berlin where he stayed with the family of a comrade.[16]
+
+The List Regiment fought in many battles, including the First Battle of Ypres (1914)'''
+    },{
+        'slug': 'German-Berlin',
+        'title': 'German.Berlin',
+        'author': 'Hooman Sepehrirad',
+        'image': 'IMG_20180619_190938.jpg',
+        'data': date(2022,1,17),
+        'short_description': 'this is all Power German in 1942',
+        'content': '''n Vienna, where he had been living in relative poverty since 1907, Hitler received the final part of his father's estate in May 1913 and moved to Munich, where he earned money painting architectural scenes. He may have left Vienna to evade conscription into the Austrian Army.[3] Hitler later claimed that he did not wish to serve the Habsburg Empire because of the mixture of "races" in its army. The Bavarian police sent him back to Salzburg for induction into the Austrian Army, but he failed his physical exam on 5 February 1914 and returned to Munich.[3]
+
+He was 25 years old in August 1914, when Austria-Hungary and the German Empire entered the First World War. 
+
+Because of his Austrian citizenship, he had to request permission to serve in the Bavarian Army. Permission was granted.[4] On the evidence of a report by the Bavarian authorities in 1924, which questioned how Hitler was allowed to serve in the Bavarian Army, Hitler almost certainly was enlisted through an error on the part of the government. The authorities could not explain why he was not deported back to Austria in 1914 after he failed his physical exam for the Austrian Army. They concluded that the matter of Hitler's citizenship was simply not raised; thus he was allowed to enter the Bavarian Army.[5] In the army, Hitler continued to put forth his German nationalist ideas which he developed from a young age.[6]
+
+During the war, Hitler served in France and Belgium in the Bavarian Reserve Infantry Regiment 16 (1st Company of the List Regiment).[7][8] He was an infantryman in the 1st Company during the First Battle of Ypres (October 1914), which Germans remember as the Kindermord bei Ypern (Ypres Massacre of the Innocents) because approximately 40,000 men (between a third and a half, many of them university students) of nine newly-enlisted infantry divisions became casualties in the first twenty days. Hitler's regiment entered the battle with 3,600 men but at its end mustered only 611 men.[9] By December, Hitler's own company of 250 was reduced to 42. Biographer John Keegan claims that this experience drove Hitler to become aloof and withdrawn for the remaining years of war.[10] After the battle, Hitler was promoted from Schütze (private) to Gefreiter (lance corporal). He was assigned to be a regimental message-runner.[11][12]
+
+Some have regarded this assignment as "a relatively safe job", the Battle of Arras (1917),
+ 
+and the Battle of Passchendaele (1917).[17] During the Battle of Fromelles on 19–20 July 1916 the Australians,
+ 
+mounting their first attack in France, assaulted the Bavarian positions. The Bavarians repulsed the attackers, who suffered the second-highest losses they had on any day on the Western Front, 
+
+about 7,000 men.[18] The history of the List Regiment hailed this brilliant defense as the "personification of the German Army on the Western Front".[19]
+
+At the Nuremberg Trials, two of his former superiors testified that Hitler had refused to be considered for promotion.[A 3] Hitler was twice decorated for bravery. He received the Iron Cross Second Class in 1914 and the Iron Cross First Class in 1918, 
+
+an honour rarely given to a lance corporal.[20] Hitler's First Class Iron Cross was recommended by Lieutenant Hugo Gutmann, a Jewish adjutant in the List Regiment.[21] According to Weber, this rare award was commonly awarded to those posted to regimental headquarters,
+
+such as Hitler, who had contact with more senior officers than did combat soldiers.[13] Hitler's Iron Cross First Class was awarded after an attack in open warfare during which messengers were indispensable and on a day in which the depleted regiment lost 60 killed and 211 wounded.[22]
+
+During the Battle of the Somme in October 1916 Hitler received a wound in his left thigh when a shell exploded at the entrance to the dispatch runners' dugout.[23] He begged not to be evacuated,
+
+[24] but was sent for almost two months to the Red Cross hospital at Beelitz in Brandenburg. Thereafter, he was ordered to the depot in Munich. He wrote to his commanding officer, 
+
+Hauptmann Fritz Wiedemann, asking that he be recalled to the regiment because he could not tolerate Munich when he knew his comrades were at the Front.[25] Wiedemann arranged for Hitler's return to his regiment on 5 March 1917.[16]
+
+On 15 October 1918, he and several comrades were temporarily blinded—and according to Friedelind Wagner he was assigned to 7th Company of the 1st Replacement Battalion of the 2nd Infantry Regiment.
+ 
+In December he was reassigned to a Prisoner of War camp in Traunstein as a guard.[31] There he would stay until the camp dissolved January 1919.[A 4]
+
+He returned to Munich and spent a few months in barracks waiting for reassignment. Munich, 
+
+then part of the People's State of Bavaria, was in a state of chaos with a number of assassinations occurring, including that of socialist Kurt Eisner[A 5] who was shot dead in Munich by a German nationalist on 21 February 1919. His rival Erhard Auer was also wounded in an attack. Other acts of violence were the killings of both Major Paul Ritter von Jahreiß and the conservative MP Heinrich Osel. In this political turmoil, Berlin sent in the military – called the "White Guards of Capitalism" by the communists. On 3 April 1919, Hitler was elected as the liaison of his military battalion and again on 15 April. During this time he urged his unit to stay out of the fighting and not join either side.[32] The Bavarian Soviet Republic was officially crushed on 6 May 1919, when Lt. General Burghard von Oven and his military forces declared the city secure. In the aftermath of arrests and executions, Hitler denounced a fellow liaison, Georg Dufter, as a Soviet "radical rabble-rouser."[33] Other testimony he gave to the military board of inquiry allowed them to root out other members of the military that "had been infected with revolutionary fervor."[34] For his anti-communist views he was allowed to avoid discharge when his unit was disbanded in May 1919.[35]'''
+
+    },{
+        'slug': 'German-Lokhzesh',
+        'title': 'German.Lokhzesh',
+        'author': 'Hooman Sepehrirad',
+        'image': 'IMG_20180619_190928.jpg',
+        'data': date(2022,9,3),
+        'short_description': 'this is first Pic Before war (1h)',
+        'content': '''the Battle of the Somme (1916), the Battle of Arras (1917), 
+        
+and the Battle of Passchendaele (1917).[17] During the Battle of Fromelles on 19–20 July 1916 the Australians, mounting their first attack in France,
+
+assaulted the Bavarian positions. The Bavarians repulsed the attackers, who suffered the second-highest losses they had on any day on the Western Front, 
+
+about 7,000 men.[18] The history of the List Regiment hailed this brilliant defense as the "personification of the German Army on the Western Front".[19]
+
+At the Nuremberg Trials, two of his former superiors testified that Hitler had refused to be considered for promotion.[A 3] Hitler was twice decorated for bravery. He received the Iron Cross Second Class in 1914 and the Iron Cross First Class in 1918, 
+
+an honour rarely given to a lance corporal.[20] Hitler's First Class Iron Cross was recommended by Lieutenant Hugo Gutmann, 
+
+a Jewish adjutant in the List Regiment.[21] According to Weber, this rare award was commonly awarded to those posted to regimental headquarters, such as Hitler,
+ 
+who had contact with more senior officers than did combat soldiers.[13] Hitler's Iron Cross First Class was awarded after an attack in open warfare during which messengers were indispensable and on a day in which the depleted regiment lost 60 killed and 211 wounded.[22]
+
+During the Battle of the Somme in October 1916 Hitler received a wound in his left thigh when a shell exploded at the entrance to the dispatch runners' dugout.[23] He begged not to be evacuated,
+
+[24] but was sent for almost two months to the Red Cross hospital at Beelitz in Brandenburg. Thereafter, he was ordered to the depot in Munich. He wrote to his commanding officer, Hauptmann Fritz Wiedemann, 
+
+asking that he be recalled to the regiment because he could not tolerate Munich when he knew his comrades were at the Front.[25] Wiedemann arranged for Hitler's return to his regiment on 5 March 1917.[16]
+
+On 15 October 1918, he and several comrades were temporarily blinded—and according to Friedelind Wagner,[26] Hitler also lost his voice—due to a British mustard gas attack. After initial treatment, 
+
+Hitler was hospitalized in Pasewalk in Pomerania.[27] While there, on 10 November, Hitler learned of Germany's defeat from a pastor, and—by his own account—on receiving this news he suffered a second bout of blindness.[28] Hitler was outraged by the subsequent Treaty of Versailles (1919), 
+
+which forced Germany to accept responsibility for starting the war, deprived Germany of various territories, demilitarised the Rhineland (which the Allies occupied), and imposed economically damaging sanctions. Hitler later wrote: "When I was confined to bed,
+ 
+the idea came to me that I would liberate Germany,that I would make it great. I knew immediately that it would be realized."[29] However, it is unlikely that he committed himself to a career in politics at that point in time.[30]
+
+On 19 November 1918, Hitler was discharged from the Pasewalk hospital and returned to Munich. Arriving on 21 November, he was assigned to 7th Company of the 1st Replacement Battalion of the 2nd Infantry Regiment. In December he was reassigned to a Prisoner of War camp in Traunstein as a guard.[31] There he would stay until the camp dissolved January 1919.[A 4]
+
+He returned to Munich and spent a few months in barracks waiting for reassignment. Munich, then part of the People's State of Bavaria, was in a state of chaos with a number of assassinations occurring, including that of socialist Kurt Eisner[A 5] who was shot dead in Munich by a German nationalist on 21 February 1919. His rival Erhard Auer was also wounded in an attack. Other acts of violence were the killings of both Major Paul Ritter von Jahreiß and the conservative MP Heinrich Osel. In this political turmoil, 
+
+Berlin sent in the military – called the "White Guards of Capitalism" by the communists. On 3 April 1919, Hitler was elected as the liaison of his military battalion and again on 15 April. During this time he urged his unit to stay out of the fighting and not join either side.[32] The Bavarian Soviet Republic was officially crushed on 6 May 1919, when Lt. General Burghard von Oven and his military forces declared the city secure. In the aftermath of arrests and executions, Hitler denounced a fellow liaison, 
+
+Georg Dufter, as a Soviet "radical rabble-rouser."[33] Other testimony he gave to the military board of inquiry allowed them to root out other members of the military that "had been infected with revolutionary fervor."[34] For his anti-communist views he was allowed to avoid discharge when his unit was disbanded in May 1919.[35]'''
+
+    }
+]
+
+
+
+
+def data_get(post):
+    return post['data']
+
+
+def index(request):
+    list_sorted = sorted(all_post,key=data_get)
+    context = list_sorted
+    return render(request, 'blog/index.html',{'post':context})
+
+
+def posts(request):
+    return render(request, 'blog/all-posts.html',{'all_post':all_post})
+
+
+def single_post(request,slug):
+    print(slug)
+    post = next(post for post in all_post if post['slug']==slug)
+    return render(request, 'blog/post-detail.html',{'post':post})
